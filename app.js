b8d001586134d45b02e11d6490a9337fbbecd837
Furkan Durak <furkandurak22@hotmail.com>
2023-04-17 17:35:01 +0300

    second

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ const add = document.querySelectorAll(".add");
 for(var i = 0; i < add.length; i++) {
     add[i].onclick = function() {
         const value = this.parentNode.childNodes[3].textContent;
-
+        let fruit = document.querySelector('.shoplist').innerHTML.includes(value);
+        if(fruit){
+            let sepet = document.querySelectorAll(".sepet .list-item .listname");
+            console.log(sepet);
+            for(var i = 0; i < sepet.length; i++) {
+                let element = sepet[i].parentNode.querySelector(".count");
+                var count = parseInt(element.innerHTML);
+                if(count < 10) {
+                    sepet[i].innerHTML == value ? count = count + 1 : count;
+                    element.innerHTML = count;
+                }
+            }
+        } else {
+            
         document.querySelector(".shoplist").innerHTML
         +=
         `
             <div class="list-item">
-                <span class="listname">
-                    ${value}
-                </span>
-                <label id="quantity">1</label>
+                <span class="listname">${value}</span>
+                <label class="count" id="quantity">1</label>
                 <button id="plus">+</button>
                 <button id="minus">-</button>
                 <button class="delete">
@@ -19,8 +30,9 @@ for(var i = 0; i < add.length; i++) {
             </div>
         `;
 
-        var listname = document.querySelectorAll(".listname").innerText;
-        console.log(listname);
+        }
+
+
 
 
 
