1881b532bd4faef6c124518e6f16f155f67e3868
Furkan Durak <furkandurak22@hotmail.com>
2023-04-18 15:57:55 +0300

    update

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ for(var i = 0; i < add.length; i++) {
     add[i].onclick = function() {
         const value = this.parentNode.childNodes[3].textContent;
         let input = document.querySelector(".mainBox .urun #urunAdi").parentNode.childNodes[1].value;
+        console.log(input);
         let fruit = document.querySelector('.shoplist').innerHTML.includes(value);
-
+        let price = this.parentNode.childNodes[5].firstChild.textContent;
         if(fruit){
             let sepet = document.querySelectorAll(".sepet .list-item .listname");
             for(var i = 0; i < sepet.length; i++) {
                 let element = sepet[i].parentNode.querySelector(".count");
+                let tutar = sepet[i].parentNode.querySelector(".tutar");
                 var count = parseInt(element.innerHTML);
+                var tutarlar = parseInt(tutar.innerHTML);
                 if(count < 10) {
-                    sepet[i].innerHTML == value ? count = count + 1 : count;
+                    if(sepet[i].innerHTML == value) {
+                        count = count + 1;
+                    } else {
+                        count;
+                    }
                     element.innerHTML = count;
+                    tutarlar = tutarlar * count;
+
                 }
             }
         } 
@@ -23,6 +32,7 @@ for(var i = 0; i < add.length; i++) {
         `
             <div class="list-item">
                 <span class="listname">${value}</span>
+                <label class="tutar" id="fiyat">${price}</label>
                 <label class="count" id="quantity">1</label>
                 <button id="plus">+</button>
                 <button id="minus">-</button>
@@ -39,6 +49,7 @@ for(var i = 0; i < add.length; i++) {
             `
                 <div class="list-item">
                     <span class="listname">${input}</span>
+                    <label class="tutar" id="fiyat">${price}</label>
                     <label class="count" id="quantity">1</label>
                     <button id="plus">+</button>
                     <button id="minus">-</button>
@@ -54,10 +65,10 @@ for(var i = 0; i < add.length; i++) {
         var plus = document.querySelectorAll("#plus");
         for(var k = 0; k < plus.length; k++) {
             plus[k].onclick = function() {
-                if(this.parentNode.childNodes[3].textContent < 10) {
-                    this.parentNode.childNodes[3].textContent++;
+                if(this.parentNode.childNodes[5].textContent < 10) {
+                    this.parentNode.childNodes[5].textContent++;
                 } else {
-                    this.parentNode.childNodes[3].textContent = 10;
+                    this.parentNode.childNodes[5].textContent = 10;
                     alert("Maksimum 10 adet alabilirsiniz!");
                 }
             }
@@ -66,9 +77,9 @@ for(var i = 0; i < add.length; i++) {
         var minus = document.querySelectorAll("#minus");
         for(var l = 0; l < minus.length; l++) {
             minus[l].onclick = function() {
-                if(this.parentNode.childNodes[3].textContent > 1) {
-                    this.parentNode.childNodes[3].textContent--;
-                } else if(this.parentNode.childNodes[3].textContent == 1) {
+                if(this.parentNode.childNodes[5].textContent > 1) {
+                    this.parentNode.childNodes[5].textContent--;
+                } else if(this.parentNode.childNodes[5].textContent == 1) {
                     this.parentNode.remove();
                 }
             }
